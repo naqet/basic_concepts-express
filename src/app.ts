5eba01022550c14c8d5f72d0dc8a22688ea2099b
Szymon Szadkowski <szymon.tomasz.szadkowski@gmail.com>
2022-11-16 19:25:00 +0700

    Added logger

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
+import logger from "./utils/logger";
 import express from "express";
 
 const app = express();
 
 app.listen(process.env.PORT || 5000, () => {
-	console.log("App is running");
+  logger.info("App is running");
 });
