9f281ba3d377df81bc87eba7eecc803db46690f2
Szymon Szadkowski <szymon.tomasz.szadkowski@gmail.com>
2022-11-21 22:11:14 +0700

    Added Post repository

---
+++ src/repositories/post/index.ts
@@ -0,0 +1,21 @@
+import { Post } from "@prisma/client";
+import IPostPayload from "../../types/IPostPayload";
+import db from "../../utils/db";
+
+export const getPost = async (id: number): Promise<Post | null> => {
+  return await db.post.findFirst({ where: { id } });
+};
+
+export const getAllPosts = async (): Promise<Post[]> => {
+  return await db.post.findMany();
+};
+
+export const createPost = async (payload: IPostPayload): Promise<Post> => {
+  return await db.post.create({
+    data: { content: payload.content, authorId: payload.authorId },
+  });
+};
+
+export const deletePost = async (id: number): Promise<Post> => {
+  return await db.post.delete({ where: { id } });
+};
---
+++ src/types/IPostPayload.ts
@@ -0,0 +1,4 @@
+export default interface IPostPayload {
+  content: string;
+  authorId: number;
+}
