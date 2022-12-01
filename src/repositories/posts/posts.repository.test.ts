83e0e79443828b9e71c107e3551d7f314a75e85d
Szymon Szadkowski <szymon.tomasz.szadkowski@gmail.com>
2022-12-01 09:47:31 +0100

    test(post-repository): Add tests

---
+++ src/repositories/posts/posts.repository.test.ts
@@ -0,0 +1,57 @@
+import {
+  generatePostData, generatePostsData,
+} from '../../utils/tests/generate';
+import { prismaMock } from '../../__mocks__/prisma';
+import {
+  getAllPosts, getPost, deletePost, createPost,
+} from './index';
+
+describe('Posts repository', () => {
+  describe('getAllPosts', () => {
+    it('it should return empty array, if no posts found', async () => {
+      prismaMock.post.findMany.mockResolvedValue([]);
+
+      await expect(getAllPosts()).resolves.toEqual([]);
+    });
+
+    it('it should return posts array', async () => {
+      const posts = generatePostsData(3);
+      prismaMock.post.findMany.mockResolvedValue(posts);
+
+      await expect(getAllPosts()).resolves.toEqual(posts);
+    });
+  });
+
+  describe('getPost', () => {
+    it('it should return null, if no post is found', async () => {
+      prismaMock.post.findFirst.mockResolvedValue(null);
+
+      await expect(getPost(1234)).resolves.toEqual(null);
+    });
+
+    it('it should return posts array', async () => {
+      const post = generatePostData();
+      prismaMock.post.findFirst.mockResolvedValue(post);
+
+      await expect(getPost(post.id)).resolves.toEqual(post);
+    });
+  });
+
+  describe('deletePost', () => {
+    it('it should return deleted post', async () => {
+      const post = generatePostData();
+      prismaMock.post.delete.mockResolvedValue(post);
+
+      await expect(deletePost(post.id)).resolves.toEqual(post);
+    });
+  });
+
+  describe('createPost', () => {
+    it('it should return a post that was added to db', async () => {
+      const post = generatePostData();
+      prismaMock.post.create.mockResolvedValue(post);
+
+      await expect(createPost({ content: post.content, authorId: post.authorId })).resolves.toEqual(post);
+    });
+  });
+});
--- src/utils/tests/generate.ts
+++ src/utils/tests/generate.ts
@@ -1,5 +1,5 @@
 import { faker } from '@faker-js/faker';
-import { User } from '@prisma/client';
+import { Post, User } from '@prisma/client';
 
 export const generateUserData = (override: any = {}): User => ({
   id: Number(faker.random.numeric(3)),
@@ -12,3 +12,14 @@ export const generateUserData = (override: any = {}): User => ({
 });
 
 export const generateUsersData = (usersNumber: number): User[] => Array.from({ length: usersNumber }, () => generateUserData());
+
+export const generatePostData = (override: any = {}): Post => ({
+  id: Number(faker.random.numeric(3)),
+  content: faker.random.words(10),
+  authorId: Number(faker.random.numeric(3)),
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  ...override,
+});
+
+export const generatePostsData = (usersNumber: number): Post[] => Array.from({ length: usersNumber }, () => generatePostData());
